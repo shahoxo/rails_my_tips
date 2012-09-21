2893c7dd346f6603ebadf756ca3eeb794d8b2032
Jun Sitow <mottipok@hotmail.com>
2012-09-21 10:49:04 +0900

    replace

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubycube::Application.config.secret_token = '5b3442d7f6b73fb9200dcb83a8206e74f19e6a29233cf4ffc182698f9742fb61af73e2daca89543c24051d303a14e9725f597947084e5ee7736a1acbc6a959cf'
