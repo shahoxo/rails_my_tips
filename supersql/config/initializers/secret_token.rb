3a325341764a577fbebecd696bb611bb8996a349
Jun Sitow <mottipok@hotmail.com>
2012-09-24 16:24:25 +0900

    rails ひな形

---
+++ supersql/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supersql::Application.config.secret_token = '1024920291413a63b879fb17432a023c66202ce8a62fe3e5599569fa12871e311f71683da583e35bb9fb3529d88985327d6f250e5f6b085123653c0c6d6bcc94'
