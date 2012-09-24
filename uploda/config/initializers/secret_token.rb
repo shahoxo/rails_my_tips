e86d8524fdba8535b3917c60962b128f10da5a00
Jun Sitow <mottipok@hotmail.com>
2012-09-24 11:39:38 +0900

    rails ひな形

---
+++ uploda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upld::Application.config.secret_token = 'abc39732f29c71132478754958e97a77a3dd075afe3b8fd956d57c2705091fa6f8b922563f434da9dc4d9024ba05302160ad79b08eafcd46c9b8fcd33bb8af40'
---
+++ uploda/db/migrate/20120924023323_create_pictures.rb
@@ -0,0 +1,7 @@
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+
+    end
+  end
+end
